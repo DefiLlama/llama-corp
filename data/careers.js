1615478df69fc3d209f4bb7b321f751403549fc3
Llama0x <73503892+Llama0x@users.noreply.github.com>
2022-09-18 11:39:54 +0100

    Update careers.js

--- data/careers.js
+++ data/careers.js
@@ -1,53 +1,123 @@
 const jobs = [
-  {
-    slug: "llamanodes-business-dev",
-    title: "Business Dev Manager",
-    project: "LlamaNodes",
+    {
+    slug: "llamafolio-front-end-dev",
+    title: "Front End Dev",
+    project: "LlamaFolio",
     description:
-      "Generate revenue for LlamaNodes and identify new business opportunities.",
+      "Build frontends and adapters for LlamaFolio.",
     content: `
       <section>
-        <h1>Business Dev Manager</h1>
-
+        <h1>Front end Developer</h1>
         <p>
-          Fast and secure access to the blockchain powered by LlamaNodes. Salary from ~$50k per year and up based on experience plus commissions.
+          Build a transparent and open source DeFi portfolio dashboard. Salary ~$50k per year and up based on experience.
         </p>
         <p>
-        LlamaNodes does Web3 infrastructure the Crypto Way. Open source, transparent and web3 first.
+        Portfolio trackers should be open and transparent with data policy that respects users.
+        We run this ecosystem so that everyone can have a fair, transparent and open-source access to crypto and web3.
         </p>
         <p>
-          Tasks:
+          Stack:
           <ul>
-            <li>Develop and shape LlamNodes from the ground up</li>
-            <li>Build and develop relations with clients</li>
-            <li>Help grow and lead the LlamaNodes offering</li>
+            <li>React</li>
+            <li>NextJS</li>
+            <li>ethers js</li>
+            <li>Nice to have: experience building dApps</li>
           </ul>
         </p>
       </section>
-
-
       <section>
         <h3>TLDR;</h3>
-
         <ul>
+          <li>Anon friendly</li>
           <li>Max autonomy and control over your work</li>
           <li>High impact</li>
           <li>No meetings, all comms are async</li>
           <li>Small team, horizontal leadership</li>
         </ul>
       </section>
-
       <section>
         <h3>Culture</h3>
-
         <p>
           What we value the most is speed of execution, and we actively try to remove anything extraneous or not needed.
         </p>
-
         <p>
           This creates an environment where we instantly extend a lot of trust to everyone (we just trust that they are actively working, we won't micro-manage to make sure you do X hours on Y days), and the following people thrive:
         </p>
-
+        <ul>
+          <li>You want to get involved in a new project with high growth prospects and plenty of responsibilities</li>
+          <li>You enjoy having full control over your work and the tools you use.</li>
+          <li>You enjoy making a large impact</li>
+          <li>You have strong initiative, if you identify something that could be improved you just go and improve it</li>
+          <li>You want to stop wasting time on things like meetings and stuff that doesn't matter and instead prefer having full and un-interrupted control of your time</li>
+          <li>You believe in what LlamaFolio is building</li>
+        </ul>
+        <p>
+          However it doesn't work for the following people:
+        </p>
+        <ul>
+          <li>We don't micro-manage so team members are expected to do their work as requested and meet their deadlines. They are trusted to deliver and self-manage.</li>
+          <li>You have low intrinsic motivation: Most people at LlamaFolio work independently, so if you don't have a strong intrinsic motivation it's very likely you'll get demotivated and stop working.</li>
+        </ul>
+      </section>
+      <section>
+        <h3>Employee benefits</h3>
+        <ul>
+          <li>Equity packages</li>
+          <li>Fully remote positions</li>
+        </ul>
+      </section>
+      <section>
+        <h3>Applying</h3>
+        <p>
+          Contact contact@llama-corp.com
+        </p>
+      </section>
+      `,
+  },
+  {
+    slug: "llamanodes-front-end-dev",
+    title: "Front End Dev",
+    project: "LlamaNodes",
+    description:
+      "Build frontends for LlamaNodes.",
+    content: `
+      <section>
+        <h1>Front end Developer</h1>
+        <p>
+          Fast and secure access to the blockchain powered by LlamaNodes. Salary ~$50k per year and up based on experience.
+        </p>
+        <p>
+        LlamaNodes does Web3 infrastructure the Crypto Way. Open source, transparent and web3 first.
+        </p>
+        <p>
+          Stack:
+          <ul>
+            <li>React</li>
+            <li>NextJS</li>
+            <li>ethers js</li>
+            <li>Web3 Sign in</li>
+            <li>Nice to have: experience building dApps</li>
+          </ul>
+        </p>
+      </section>
+      <section>
+        <h3>TLDR;</h3>
+        <ul>
+          <li>Anon friendly</li>
+          <li>Max autonomy and control over your work</li>
+          <li>High impact</li>
+          <li>No meetings, all comms are async</li>
+          <li>Small team, horizontal leadership</li>
+        </ul>
+      </section>
+      <section>
+        <h3>Culture</h3>
+        <p>
+          What we value the most is speed of execution, and we actively try to remove anything extraneous or not needed.
+        </p>
+        <p>
+          This creates an environment where we instantly extend a lot of trust to everyone (we just trust that they are actively working, we won't micro-manage to make sure you do X hours on Y days), and the following people thrive:
+        </p>
         <ul>
           <li>You want to get involved in a new project with high growth prospects and plenty of responsibilities</li>
           <li>You enjoy having full control over your work and the tools you use.</li>
@@ -56,36 +126,29 @@ const jobs = [
           <li>You want to stop wasting time on things like meetings and stuff that doesn't matter and instead prefer having full and un-interrupted control of your time</li>
           <li>You believe in what LlamaNodes is building</li>
         </ul>
-
         <p>
           However it doesn't work for the following people:
         </p>
-
         <ul>
           <li>We don't micro-manage so team members are expected to do their work as requested and meet their deadlines. They are trusted to deliver and self-manage.</li>
           <li>You have low intrinsic motivation: Most people at LlamaNodes work independently, so if you don't have a strong intrinsic motivation it's very likely you'll get demotivated and stop working.</li>
         </ul>
       </section>
-
       <section>
         <h3>Employee benefits</h3>
-
         <ul>
-          <li>Performance based bonuses</li>
           <li>Equity packages</li>
           <li>Fully remote positions</li>
         </ul>
       </section>
-
       <section>
         <h3>Applying</h3>
-
         <p>
           Contact contact@llama-corp.com
         </p>
       </section>
       `,
-  },
+  }
 ];
 
 export default jobs;
