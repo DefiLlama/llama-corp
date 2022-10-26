13f74599c027cfb470492f6ee3d5a197c490c598
charlie-eth <charlie_eth+main@protonmail.ch>
2022-10-26 11:29:29 +0100

    add dl news, remove job posts

--- data/projects.js
+++ data/projects.js
@@ -11,17 +11,6 @@ const projects = [
     href: "https://defillama.com"
   },
   {
-    title: "LlamaPay",
-    description:
-      "LlamaPay is a non-custodial solution to automate recurring payments like salaries for crypto companies.",
-    logo: {
-      src: "/assets/llamapay.svg",
-      width: 220,
-      height: 60,
-    },
-    href: "https://llamapay.io/"
-  },
-  {
     title: "LlamaNodes",
     description:
       "LlamaNodes offers fast and secure access to the blockchain. Robust, private, and open-source: LlamaNodes does Web3 infrastructure the cryptoway.",
@@ -32,6 +21,29 @@ const projects = [
     },
     href: "https://twitter.com/LlamaNodes"
   },
+  {
+    title: "DLNews",
+    description:
+      "DLNews is a news organisation that provides original, in-depth reporting on cryptocurrency and decentralised finance.",
+    logo: {
+      src: "/assets/dlnews.svg",
+      width: 270,
+      height: 60,
+    },
+    href: "https://dlnews.com"
+  },
+  // {
+  //   title: "LlamaPay",
+  //   description:
+  //     "LlamaPay is a non-custodial solution to automate recurring payments like salaries for crypto companies.",
+  //   logo: {
+  //     src: "/assets/llamapay.svg",
+  //     width: 220,
+  //     height: 60,
+  //   },
+  //   href: "https://llamapay.io/"
+  // },
+
   {
     title: "LlamaFolio",
     description:
