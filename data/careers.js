8da5dca1e79fbdad15ee4414ca856ce81f6c9992
charlie-eth <charlie_eth+main@protonmail.ch>
2022-12-05 15:44:22 +0000

    add new position

--- data/careers.js
+++ data/careers.js
@@ -1,5 +1,68 @@
 const jobs = [
-    
+  {
+  slug: "llamanodes-dev-ops-engineer",
+  title: "Dev Ops Engineer",
+  project: "LlamaNodes",
+  description:
+    "Build the future of web3 infra with LlamaNodes.",
+  content: `
+    <section>
+      <h1>Dev Ops Engineer</h1>
+      <p>
+      LlamaNodes provides RPC services for web3 native teams with industry-leading features, crypto payments, and no contracts. Our vision is to provide unwavering and democratic access to blockchains with the best features possible, at the lowest price.
+      </p>
+      <p>
+      LlamaNodes is part of LlamaCorp, a team of passionate and skilled individuals driven by the mission of providing web3 infrastructure that is fully open-source and transparent.
+      </p>
+      <h3>Role</h3>
+
+      <p>
+      You will work directly with our CTO and founders to realise the LlamaNodes vision. In this role you’ll be taking a leading position within a small and driven team to expand, improve, and create new ways to manage all our deployed RPC services and products. In this agile environment, you’ll have the autonomy to design the best solutions to complete the task at hand. Our ideal candidate has substantial infrastructure experience in a range of environments and an understanding of EVMs and Web3. We are fully remote, and pay real salaries with upside related to the success of the project.
+      </p>
+    </section>
+    <section>
+      <h3>Functional Competencies </h3>
+      <ul>
+      <li> 2+ years as a sysadmin, systems engineer, site reliability engineer, reliability engineer, or related role in a professional setting.</li>
+      <li> Experience with deploying and maintaining full nodes in production environments, with CI/CD in mind to support multiple environments and networks in multiple worldwide locations.</li>
+<li> Experience working with automation and infrastructure tooling like Docker, Terraform, Ansible, Packer, Consul.</li>
+<li> Experience designing and maintaining production-ready workloads using software-defined cloud infrastructure, especially security policies and access control (AWS, GCP, Azure, etc).</li>
+<li> Ability to showcase experience in building reliable software infrastructure, specifically for Rust and load balancing in Haproxy.</li>
+<li> Owning alerting systems and processes Pagerduty, Graphana, Prometheus.</li>
+<li> Languages/frameworks (e.g. Rust).</li>
+<li> Manually managing much of the above responsibilities and automating them to allow the organisation to continuously expand the scope of these activities.</li>
+      </ul>
+    </section>
+    <section>
+      <h3>Behavioural Competencies </h3>
+      <p>
+
+      <ul>
+        <li>A love for DeFi | You are enamoured with cryptocurrency, web3 and blockchain-based technologies.</li>
+        <li>Self-motivated | Relentlessly focused on impact, and able to operate under a great deal of autonomy. </li>
+        <li>Excellent English | Communication skills, both written and verbal with the ability to balance communications multiple timezones. </li>
+      </ul>
+
+    </section>
+    <section>
+      <h3>Benefits of The Llama </h3>
+      <ul>
+        <li>🌎 Remote First | LlamaNodes is remote-first with team members from across the world.</li>
+        <li>💻 Work Set-Up | We’ll provide technology stipend of $1500 USD </li>
+        <li>🧘‍♀️ Flexible hours | Help you navigate around other responsibilities in life.</li>
+        <li>💰 Compensation Package | We offer competitive compensation and equity. The success of our project has a direct correlation with your net worth. </li>
+        <li>📚 Continuing Education | Want to brush up on your skills? We’ll help pay for any education that helps you excel in your role. </li>
+        <li>🦙 Access to the Llama |  As a part of LlamaCorp, you can work with a variety of Llama projects and get experience from the best in the industry.  </li>
+      </ul>
+    </section>
+    <section>
+      <h3>How to Apply </h3>
+      <p>
+      Email: info@llamanodes.com with your CV attached. Subject line: “Dev Ops Role LlamaNodes”
+      </p>
+    </section>
+    `,
+  }
 ];
 
 export default jobs;
