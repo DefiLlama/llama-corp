25166478a35525b0287be0215ae6537985be7ef1
0xsign <0xsign@protonmail.com>
2023-01-18 22:55:48 +0100

    fix: team grid styles

--- components/team.js
+++ components/team.js
@@ -34,27 +34,29 @@ function Card({ name, role, img, twitter, github, telegram, projects }) {
           <Image alt={name} src={img} layout="fill" objectFit="cover" />
         </div>
 
-        <div>
-          <p className="name">{name}</p>
-          <p className="role">{role}</p>
-        </div>
+        <div className="col">
+          <div>
+            <p className="name">{name}</p>
+            <p className="role">{role}</p>
 
-        <div className="grid-2">
-          {_projects.map(({ id, title }) => (
-            <div className="pill" key={`project__${id}`}>
-              {title}
+            <div className="row">
+              {_projects.map(({ id, title }) => (
+                <div className="pill" key={`project__${id}`}>
+                  {title}
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </div>
 
-        <div className="row">
-          {mediaLinks.map(({ Icon, href }, idx) => (
-            <Link key={`mediaLink__${idx}`} href={href}>
-              <a target="_blank" rel="noreferrer noopener">
-                <Icon size={18} />
-              </a>
-            </Link>
-          ))}
+          <div className="row-social">
+            {mediaLinks.map(({ Icon, href }, idx) => (
+              <Link key={`mediaLink__${idx}`} href={href}>
+                <a target="_blank" rel="noreferrer noopener" className="link">
+                  <Icon size={14} />
+                </a>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
 
@@ -66,61 +68,74 @@ function Card({ name, role, img, twitter, github, telegram, projects }) {
           border-radius: 12px;
         }
 
+        .col {
+          height: 140px;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+        }
+
         .img-container {
           position: relative;
           width: 100%;
-          height: 200px;
-          border-radius: 12px;
+          height: 130px;
+          border-radius: 8px;
           overflow: hidden;
-          border-radius: 50%;
-
         }
 
         .name {
-          margin: 10px;
-          font-weight: 600;
-          font-size: 1.2rem;
-          line-height: 25px;
+          margin: 5px 0 0 0;
+          font-weight: bold;
+          font-size: 1rem;
         }
 
         .role {
-          margin-top: 5px;
+          margin-top: 0px;
           font-weight: 400;
-          font-size: 1rem;
-          line-height: 19px;
-        }
-
-        .grid-2 {
-          display: flex;
-          flex-direction: row;
-          flex-wrap: wrap;
-          justify-content: center;
-          gap: 0.5rem;
-        }
-
-        .pill {
-          background: #494d5c;
-          padding: 5px 10px;
-          width: fit-content;
-          border-radius: 12px;
-          outline: none;
-          color: #fff;
           font-size: 0.7rem;
         }
 
         .row {
+          display: flex;
+          flex-wrap: wrap;
+          align-items: center;
+          justify-content: center;
+          flex-direction: row;
+          margin-left: -0.4rem;
+          margin-top: -0.4rem;
+        }
+
+        .row > * {
+          margin-left: 0.4rem;
+          margin-top: 0.4rem;
+        }
+
+        .pill {
+          background: #494d5c;
+          padding: 2px 5px;
+          width: fit-content;
+          border-radius: 12px;
+          outline: none;
+          color: #fff;
+          font-size: 0.6rem;
+        }
+
+        .row-social {
           margin-top: 10px;
           display: flex;
           flex-direction: row;
           justify-content: center;
+          margin-left: -1rem;
+          margin-top: -1rem;
         }
 
-        .row > * {
+        .row-social > * {
           margin-left: 1rem;
+          margin-top: 1rem;
         }
 
-        .row > :first-child {
-          margin-left: 0;
+        .link:hover {
+          opacity: 0.8;
         }
       `}</style>
     </>
@@ -166,29 +181,29 @@ export default function Team({ items }) {
         {`
           .grid {
             display: grid;
-            grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
-            grid-gap: 1rem;
+            grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+            grid-gap: 0.7rem;
           }
 
           .row {
             margin: 10px 0 20px 0;
             display: flex;
+            flex-wrap: wrap;
             flex-direction: row;
+            margin-left: -1rem;
+            margin-top: -1rem;
           }
 
           .row > * {
             margin-left: 1rem;
-          }
-
-          .row > :first-child {
-            margin-left: 0;
+            margin-top: 1rem;
           }
 
           .pill {
             background: #494d5c;
-            padding: 5px 10px;
+            padding: 3px 10px;
             width: fit-content;
-            border-radius: 12px;
+            border-radius: 20px;
             outline: none;
             cursor: pointer;
             color: #fff;
